3d6860513d309d272e5ba3255127c0b2a1798b90
511 <fqd511@gmail.com>
2022-04-09 21:02:53 +0800

    feat: feat: style and order
    * modify scroll style;
    * support custom order;
    * remove TagIdEnum;

--- src/model/type.ts
+++ src/model/type.ts
@@ -1,12 +1,7 @@
 type Nullable<T> = T | null;
-export enum TagIdEnum {
-  forked = "krG?",
-  part_time = "77eb608d-e1c0-466f-9c20-ed4425a3f011",
-  side_project = "3b63b0f4-7cdd-4cbd-b745-652f2b6478c6",
-}
 
 export interface TagEnum {
-  id: TagIdEnum;
+  id: string;
   label: string;
   color: string;
 }
@@ -14,6 +9,7 @@ export interface TagEnum {
 export interface IContentItem {
   id: string;
   isHidden: boolean;
+  order: number;
   link: Nullable<string>;
   label_cn: string;
   label_en: string;
--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,27 +1,30 @@
-import { IContentItem, TagEnum, TagIdEnum } from "~/model/type";
+import { IContentItem, TagEnum } from "~/model/type";
 
 export function parserContent(raw: any[]): IContentItem[] {
-  return raw.map(
-    (item: any) =>
-      ({
-        id: item.properties.Name.title[0].plain_text,
-        isHidden: item.properties.hidden.checkbox,
-        link: item.properties.link.url,
-        label_cn: item.properties.label_cn.rich_text[0]?.plain_text,
-        label_en: item.properties.label_en.rich_text[0]?.plain_text,
-        desc_cn: item.properties.desc_cn.rich_text[0]?.plain_text,
-        desc_en: item.properties.desc_en.rich_text[0]?.plain_text,
-        thumbnail: item.properties.thumbnail.url,
-        thumbnail_fallback: item.properties.thumbnail_fallback.url,
-        deploy_hook: item.properties.deploy_hook.url,
-        tags: item.properties.tags.multi_select.map(
-          (tag: any) =>
-            ({
-              id: tag.id as TagIdEnum,
-              label: tag.name,
-              color: tag.color,
-            } as TagEnum)
-        ),
-      } as IContentItem)
-  );
+  return raw
+    .map(
+      (item: any) =>
+        ({
+          id: item.properties.Name.title[0].plain_text,
+          isHidden: item.properties.hidden.checkbox,
+          link: item.properties.link.url,
+          order: item.properties.order.number,
+          label_cn: item.properties.label_cn.rich_text[0]?.plain_text,
+          label_en: item.properties.label_en.rich_text[0]?.plain_text,
+          desc_cn: item.properties.desc_cn.rich_text[0]?.plain_text,
+          desc_en: item.properties.desc_en.rich_text[0]?.plain_text,
+          thumbnail: item.properties.thumbnail.url,
+          thumbnail_fallback: item.properties.thumbnail_fallback.url,
+          deploy_hook: item.properties.deploy_hook.url,
+          tags: item.properties.tags.multi_select.map(
+            (tag: any) =>
+              ({
+                id: tag.id,
+                label: tag.name,
+                color: tag.color,
+              } as TagEnum)
+          ),
+        } as IContentItem)
+    )
+    .sort((a, b) => a.order - b.order);
 }
