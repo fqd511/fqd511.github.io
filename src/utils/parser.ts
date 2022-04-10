1f8f024cf7079b0c4a692573dd391e123cbc7b5f
511 <fqd511@gmail.com>
2022-04-10 19:39:53 +0800

    fix: parser rename

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -10,13 +10,13 @@ export function parserContent(raw: any[]): IContentItem[] {
           isHidden: item.properties.hidden.checkbox,
           link: item.properties.link.url,
           order: item.properties.order.number,
-          labelCn: item.properties.labelCn.rich_text[0]?.plain_text,
-          labelEn: item.properties.labelEn.rich_text[0]?.plain_text,
-          descCn: item.properties.descCn.rich_text[0]?.plain_text,
-          descEn: item.properties.descEn.rich_text[0]?.plain_text,
+          labelCn: item.properties.label_cn.rich_text[0]?.plain_text,
+          labelEn: item.properties.label_en.rich_text[0]?.plain_text,
+          descCn: item.properties.desc_cn.rich_text[0]?.plain_text,
+          descEn: item.properties.desc_en.rich_text[0]?.plain_text,
           thumbnail: item.properties.thumbnail.url,
-          thumbnailFallback: item.properties.thumbnailFallback.url,
-          deployHook: item.properties.deployHook.url,
+          thumbnailFallback: item.properties.thumbnail_fallback.url,
+          deployHook: item.properties.deploy_hook.url,
           tags: item.properties.tags.multi_select.map(
             (tag: any) =>
               ({
